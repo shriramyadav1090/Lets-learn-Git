ca597d4b1d451f8135154cf7e4455d500758a4f8
Shriram Yadav <shriramyadav2020@gmail.com>
2023-05-31 13:22:49 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,11 @@
+public class Sum
+{
+   public satic void main(String[] args)
+    {
+       int a=5;
+       int b=12;
+       int c=a+b;
+       Sysem.out.println("Sum of the umbers is :" +c)
+
+    }
+}
