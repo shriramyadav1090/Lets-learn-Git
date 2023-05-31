d2691435a267e594b4fcd4f42a4f2b108e4f723a
Shriram Yadav <shriramyadav2020@gmail.com>
2023-05-31 14:33:10 +0530

    added Multiplication.java

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+public class Multiplication
+{
+   public satic void main(String[] args)
+    {
+       int a=5;
+       int b=12;
+       
+       int c=a*b;
+       
+         Sysem.out.println("Multiplication of the numbers is :" +c)
+    }
+}
